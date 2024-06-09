fa12f60960dbb583c9a8d482b536f7d98717dd50
Mtjzsa <mate.jozsa03@gmail.com>
2024-06-09 15:33:49 +0200

    list games component

--- KFrontend/Frontend/src/app/list-games/list-games.component.ts
+++ KFrontend/Frontend/src/app/list-games/list-games.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { Game } from '../_models/game';
 
 @Component({
   selector: 'app-list-games',
   templateUrl: './list-games.component.html',
   styleUrls: ['./list-games.component.scss']
 })
-export class ListGamesComponent {
+export class ListGamesComponent implements OnInit{
+  http: HttpClient
+  games: Array<Game>
 
+  
+  constructor(http: HttpClient) {
+    this.http = http,
+    this.games = []
+  }
+  ngOnInit(): void {
+    this.http.get<Array<Game>>('http://localhost:5146/Game')
+    .subscribe(resp => {
+      resp.map(x=>{
+        let s = new Game()
+        s.id = x.id
+        s.gameName = x.gameName
+        s.gameCover = x.gameCover
+        s.releaseDate = x.releaseDate
+        s.bosses = x.bosses
+        this.games.push(s)
+      })
+      console.log(this.games)
+    })
+  }
 }
