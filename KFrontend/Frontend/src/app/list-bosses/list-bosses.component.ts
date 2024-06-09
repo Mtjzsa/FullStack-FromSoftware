dbc3649e88568834d6afd5ef49426b1d20c684b4
Mtjzsa <mate.jozsa03@gmail.com>
2024-06-09 16:40:55 +0200

    list bosses component

--- KFrontend/Frontend/src/app/list-bosses/list-bosses.component.ts
+++ KFrontend/Frontend/src/app/list-bosses/list-bosses.component.ts
@@ -1,4 +1,7 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { Boss } from '../_models/boss';
+import { Game } from '../_models/game';
 
 @Component({
   selector: 'app-list-bosses',
@@ -6,5 +9,55 @@ import { Component } from '@angular/core';
   styleUrls: ['./list-bosses.component.scss']
 })
 export class ListBossesComponent {
+  http: HttpClient
+  bosses: Array<Boss>
+  games: Array<Game>
+  
+  constructor(http: HttpClient) {
+    this.http = http,
+    this.bosses = [],
+    this.games = []
+  }
+  ngOnInit(): void {
+    this.http.get<Array<Boss>>('http://localhost:5146/Boss')
+    .subscribe(resp => {
+      resp.map(x=>{
+        let s = new Boss()
+        s.id = x.id
+        s.bossName = x.bossName
+        s.game_Id = x.game_Id
+        s.hp = x.hp
+        s.defense = x.defense
+        s.image = x.image
+        s.location = x.location
+        s.souls = x.souls
+        this.bosses.push(s)
+      })
+      console.log(this.bosses)
+    })
 
+    this.http.get<Array<Game>>('http://localhost:5146/Game')
+    .subscribe(resp => {
+      resp.map(x=>{
+        let s = new Game()
+        s.id = x.id
+        s.gameName = x.gameName
+        s.gameCover = x.gameCover
+        s.releaseDate = x.releaseDate
+        s.bosses = x.bosses
+        this.games.push(s)
+      })
+      console.log(this.games)
+    })
+  }
+
+
+  public getGame(boss : Boss, games : Array<Game>) : string {
+    for (let i = 0; i < games.length; i++) {
+      if (boss.game_Id === games[i].id){
+        return games[i].gameName
+      }
+    }
+    return "No game"
+  }
 }
