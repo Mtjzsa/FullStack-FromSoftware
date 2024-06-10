c501b384fe1a25143872af60cbd494f1e1397760
Mtjzsa <mate.jozsa03@gmail.com>
2024-06-10 19:46:17 +0200

    appuser model created

---
+++ KFrontend/Frontend/src/app/_models/appuser.ts
@@ -0,0 +1,7 @@
+export class AppUser{
+  public userName : string = ""
+  public firstName : string = ""
+  public lastName : string = ""
+  public email : string = ""
+  public defeatedBosses : string = ""
+}
