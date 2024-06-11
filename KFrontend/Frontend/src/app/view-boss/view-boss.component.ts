ca0cfce336173faa79ce5961c252c57d18f49646
Mtjzsa <mate.jozsa03@gmail.com>
2024-06-11 15:23:24 +0200

    view boss component finished

--- KFrontend/Frontend/src/app/view-boss/view-boss.component.ts
+++ KFrontend/Frontend/src/app/view-boss/view-boss.component.ts
@@ -1,8 +1,9 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { MatSnackBar } from '@angular/material/snack-bar';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Boss } from '../_models/boss';
+import { Game } from '../_models/game';
 
 
 @Component({
@@ -14,16 +15,18 @@ export class ViewBossComponent implements OnInit{
   
   http: HttpClient
   route: ActivatedRoute
+  router : Router
   snackBar:MatSnackBar
   boss : Boss
-  deleteDisabled: boolean
+  games: Array<Game>
 
-  constructor(http: HttpClient, route:ActivatedRoute, snackBar:MatSnackBar) {
+  constructor(http: HttpClient, route:ActivatedRoute, snackBar:MatSnackBar, router : Router) {
     this.http = http
     this.route = route
     this.boss = new Boss()
     this.snackBar = snackBar
-    this.deleteDisabled = true
+    this.games = []
+    this.router = router
   }
   ngOnInit(): void {
     this.route.params.subscribe(param =>{
@@ -47,5 +50,24 @@ export class ViewBossComponent implements OnInit{
       })
         console.log(this.boss)
     })
+
+    this.http.get<Array<Game>>('http://localhost:5146/Game')
+    .subscribe(resp => {
+      resp.map(x=>{
+        let s = new Game()
+        s.id = x.id
+        s.gameName = x.gameName
+        s.gameCover = x.gameCover
+        s.releaseDate = x.releaseDate
+        s.bosses = x.bosses
+        s.gameDescription = x.gameDescription
+        this.games.push(s)
+      })
+    })
+  }
+
+  public getGame(boss : Boss, games : Array<Game>) : string {
+    const game = games.find(game => game.id === boss.game_Id);
+    return game ? game.gameName : 'No game';
   }
 }
